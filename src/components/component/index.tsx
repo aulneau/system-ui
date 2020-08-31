e749bac0c5e1b97b4bf6f42098a91be7a944aca3
Thomas Osmonson <aulneau@users.noreply.github.com>
2020-08-31 15:31:59 -0500

    fix: shouldForwardProp

--- src/components/component/index.tsx
+++ src/components/component/index.tsx
@@ -4,21 +4,16 @@ import { As, PropsWithAs } from '../..';
 import { useCss } from '../../hooks/use-css';
 // @ts-ignore
 import { createShouldForwardProp } from '@styled-system/should-forward-prop';
-import styled from '@emotion/styled';
 import { allPossibleProps } from '../..';
 
-const BaseComponent = <SystemProps, ComponentType extends As = 'div'>({
+const shouldForwardProp = createShouldForwardProp([...allPossibleProps]);
+
+export const Base = <SystemProps, ComponentType extends As = 'div'>({
   as,
   ...props
 }: PropsWithAs<SystemProps, ComponentType>) => {
   const Component = as || 'div';
 
   const [styles, allProps] = useCss(props);
-  return <Component css={theme => css(styles(theme))} {...allProps} />;
+  return <Component css={theme => css(styles(theme))} {...shouldForwardProp(allProps)} />;
 };
-
-const shouldForwardProp = createShouldForwardProp([...allPossibleProps]);
-
-export const Base = styled(BaseComponent)({
-  shouldForwardProp,
-});
